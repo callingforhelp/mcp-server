7ac3ee6d74a9d97b5e439c52fd160eee654c4343
callingforhelp <120263678+callingforhelp@users.noreply.github.com>
2025-06-27 17:35:25 +0800

    Add Qdrant API routes and update .env.local

--- lib/qdrant.ts
+++ lib/qdrant.ts
@@ -1,5 +1,8 @@
-import { QdrantClient } from '@qdrant/js-client-rest';
+import { QdrantClient } from "@qdrant/js-client-rest";
 
-const qdrantClient = new QdrantClient({ url: process.env.QDRANT_URL });
+const client = new QdrantClient({
+  url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY,
+});
 
-export default qdrantClient;
+export default client;
