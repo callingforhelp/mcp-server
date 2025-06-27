5ebb99440ca69bb7146e6d5ba21858dfd90c8c28
callingforhelp <120263678+callingforhelp@users.noreply.github.com>
2025-06-27 16:51:39 +0800

    Revert to clean state after debugging

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log('Hello API Endpoint - Reached!');
-  res.status(200).json({ message: 'Hello from the root API!', method: req.method });
-}
