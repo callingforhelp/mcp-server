5b67adbcbacbbb9026bcc5499c9606fb5459729b
callingforhelp <120263678+callingforhelp@users.noreply.github.com>
2025-06-27 14:49:41 +0800

    Fix middleware to handle JWT

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { jwtVerify } from 'jose';
 
-export function middleware(req: NextRequest) {
-  const apiKey = req.headers.get('authorization');
+export async function middleware(req: NextRequest) {
+  const authHeader = req.headers.get('authorization');
 
-  if (apiKey !== `Bearer ${process.env.INTERNAL_API_KEY}`) {
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return new NextResponse(
       JSON.stringify({ success: false, message: 'authentication failed' }),
       { status: 401, headers: { 'content-type': 'application/json' } }
-    )
+    );
   }
 
-  return NextResponse.next();
+  const token = authHeader.split(' ')[1];
+
+  try {
+    await jwtVerify(token, new TextEncoder().encode(process.env.INTERNAL_API_KEY));
+    return NextResponse.next();
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: 'authentication failed' }),
+      { status: 401, headers: { 'content-type': 'application/json' } }
+    );
+  }
 }
 
 export const config = {
